d30de9085c3bcee8f4e9408bb9786cb4888c27fc
Kristy Lee <leekristy01@gmail.com>
2020-07-15 14:29:29 -0400

    added jquery for the hamburger dropdown

---
+++ project/about-me/about-me.js
@@ -0,0 +1,11 @@
+let $burger = $('.burger')
+$burger.on('click', function(e) {
+    let $right = $('.right-burger');
+    $right.toggleClass('show');
+})
+
+$(window).resize (function(e) {
+   if($(window).width()<600){
+       $('.right-burger').removeClass('show')
+   }
+})

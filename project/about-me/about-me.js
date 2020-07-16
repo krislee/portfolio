ee6b34c2a80b93a5e210db70a53840aa42a376c8
Kristy Lee <leekristy01@gmail.com>
2020-07-16 17:38:46 -0400

    added sliding up for skills section in about me page

--- project/about-me/about-me.js
+++ project/about-me/about-me.js
@@ -10,20 +10,65 @@ $(window).resize (function(e) {
    }
 })
 
-let options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0
+const slideUp = () => {
+    let options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0
+    }
+    
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            console.log(entry)
+            if(entry.isIntersecting) {
+                document.querySelector('.about').setAttribute('id','slide-up')
+                document.querySelector('.profile').setAttribute('id', 'profile-appear')
+            } 
+            // else {
+            //     document.querySelector(selector1).setAttribute('id', 'slide-up-skills')
+            //     document.querySelectorAll(selector2)[0].setAttribute('id', 'icons-appear')
+            //     document.querySelectorAll(selector2)[1].setAttribute('id', 'icons-appear')
+            // }
+        })
+    }, options)
+    // let about = document.querySelector(selector1)
+    // observer.observe(about)
+    let about = document.querySelector('.about')
+    observer.observe(about)
 }
+slideUp()
 
-let observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting) {
-            document.querySelector('.about').setAttribute('id','slide-up')
-            document.querySelector('.profile').setAttribute('id', 'profile-appear')
-        }
-    })
-}, options)
-let about = document.querySelector('.about')
-observer.observe(about)
+
+const slideUp2 = () => {
+    let options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0
+    }
+    
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            console.log(entry)
+            if (entry.isIntersecting) {
+                document.querySelector('.skills-outerdiv').setAttribute('id', 'slide-up-skills')
+                document.querySelectorAll('.flex-icon')[0].setAttribute('id', 'icons-appear')
+                document.querySelectorAll('.flex-icon')[1].setAttribute('id', 'icons-appear')
+            }
+        })
+    }, options)
+    let about = document.querySelector('.skills-outerdiv')
+    observer.observe(about)
+}
+slideUp2()
+
+// Credit to StackOverflow: https://stackoverflow.com/questions/13250325/show-hide-div-on-scroll
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 800) {
+      $('.about').fadeOut();
+    //   $('.skills-outerdiv').fadeIn();
+    } else {
+      $('.about').fadeIn();
+    //   $('.skills-outerdiv').fadeOut();
+    }
+  });
+

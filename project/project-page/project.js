16a0f6fd90b4bae9578b640892f253d15acf59fb
Kristy Lee <leekristy01@gmail.com>
2020-07-16 22:20:42 -0400

    added opacity change when user scrolls for project page

--- project/project-page/project.js
+++ project/project-page/project.js
@@ -28,7 +28,89 @@ fetch(url)
     console.log(arr)
     
     create(arr)
+    let projectDiv1 = document.querySelectorAll('.container')[0]
+    let projectDiv2 = document.querySelectorAll('.container')[1]
+    let projectDiv3 = document.querySelectorAll('.container')[2]
+    let projectDiv4 = document.querySelectorAll('.container')[3]
+    let projectDiv5 = document.querySelectorAll('.container')[4]
+
+    let a = opacityChange(0)
+    let b = opacityChange(1)
+    let c = opacityChange(2)
+    let d = opacityChange(3)
+    let e = opacityChange(4)
+
+    a.observe(projectDiv1)
+    b.observe(projectDiv2) 
+    c.observe(projectDiv3)
+    d.observe(projectDiv4) 
+    e.observe(projectDiv5)
+
+    // let y = opacityChange2()
+    // y.observe(projectDiv3)
+    // y.observe(projectDiv4) 
+
+    // let z = opacityChange3()
+    // z.observe(projectDiv5)
 })
+
+const opacityChange = (num) => {
+    let options = {
+        root: null,
+        rootMargin: '0px',
+        threshold:0.8
+    }
+
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            console.log(entry)
+            if (entry.isIntersecting) {
+                document.querySelectorAll('.container')[num].setAttribute('id', 'projects-opacity')
+            }
+        })
+    }, options)
+   return observer
+}
+
+// const opacityChange2 = () => {
+//     let options = {
+//         root: null,
+//         rootMargin: '0px',
+//         threshold:0.8
+//     }
+
+//     let observer = new IntersectionObserver((entries, observer) => {
+//         entries.forEach((entry) => {
+//             console.log(entry)
+//             if (entry.isIntersecting) {
+//                 document.querySelectorAll('.container')[2].setAttribute('id', 'projects-opacity')
+//                 document.querySelectorAll('.container')[3].setAttribute('id', 'projects-opacity')
+//             }
+//         })
+//     }, options)
+//    return observer
+// }
+
+// const opacityChange3 = () => {
+//     let options = {
+//         root: null,
+//         rootMargin: '0px',
+//         threshold:0.8
+//     }
+
+//     let observer = new IntersectionObserver((entries, observer) => {
+//         entries.forEach((entry) => {
+//             console.log(entry)
+//             if (entry.isIntersecting) {
+//                 document.querySelectorAll('.container')[4].setAttribute('id', 'projects-opacity')
+//             }
+//         })
+//     }, options)
+//    return observer
+// }
+
+
+
 
 const create  = (array) => {
     array.forEach ((eachArrayElement) => {
@@ -51,3 +133,8 @@ const create  = (array) => {
     
     })
 }
+
+document.querySelectorAll('.container')[1].setAttribute('id', 'projects-opacity')
+document.querySelectorAll('.container')[2].setAttribute('id', 'projects-opacity')
+document.querySelectorAll('.container')[3].setAttribute('id', 'projects-opacity')
+document.querySelectorAll('.container')[4].setAttribute('id', 'projects-opacity')

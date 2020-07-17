6d3810b62d09a47dfeeaed19b77491d6902022b3
Kristy Lee <leekristy01@gmail.com>
2020-07-17 16:16:24 -0400

    switched hamburger icon to x icon after clicking

--- project/app.js
+++ project/app.js
@@ -2,11 +2,13 @@ let $burger = $('.burger')
 $burger.on('click', function(e) {
     let $right = $('.right-burger');
     $right.toggleClass('show');
+    $('.burger').toggleClass('xcross')
 })
 
 $(window).resize (function(e) {
    if($(window).width()<600){
        $('.right-burger').removeClass('show')
+       $('.burger').removeClass('xcross')
    }
 })
 
